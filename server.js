c20662ce18f3937127ddd17b2ce19163bd71a88e
Patrick Furtak <patrick@patrickfurtak.com>
2020-10-10 16:02:28 -0400

    Removed client side console logs, prepped node server for heroku deployment

--- server.js
+++ server.js
@@ -22,6 +22,16 @@ io.on('connection', (socket) => {
   });
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+
 http.listen(5000, () => {
   console.log('Server listening on port 5000...');
 });
